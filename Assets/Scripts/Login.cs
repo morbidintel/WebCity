b7c6e9de44f87c0e25649cdd0047fb6afe62d69d
George Chew <hanged94@gmail.com>
2018-06-20 21:59:16 +0800

    Finished with Register and Forget Password

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -9,9 +9,7 @@ public class Login : MonoBehaviour
 	string loginURL = "http://webcity.online/live/db/login.php?username={0}&pwhash={1}";
 
 	[SerializeField]
-	InputField username = null;
-	[SerializeField]
-	InputField password = null;
+	InputField username = null, password = null;
 	[SerializeField]
 	Text errorSubtitle = null;
 
@@ -19,27 +17,27 @@ public class Login : MonoBehaviour
 	ChangeScene sceneChanger = null;
 
 	[Serializable]
-	class User
+	public class User
 	{
 		public string userid, username, fbid, email, desc, isfbonly, createddate, forgetpwtime;
 		public string error;
 	}
 
-	IEnumerator GetCredential()
+	IEnumerator LoginCoroutine()
 	{
 		errorSubtitle.text = "";
 		string encodedpw = Convert.ToBase64String(Encoding.UTF8.GetBytes(password.text));
 		string url = string.Format(loginURL, WWW.EscapeURL(username.text), WWW.EscapeURL(encodedpw));
-		WWW getLogin = new WWW(url);
-		yield return getLogin;
+		WWW www = new WWW(url);
+		yield return www;
 
-		if (getLogin.error != null)
+		if (www.error != null)
 		{
-			errorSubtitle.text = getLogin.error;
+			errorSubtitle.text = www.error;
 		}
 		else
 		{
-			User json = JsonUtility.FromJson<User>(getLogin.text);
+			User json = JsonUtility.FromJson<User>(www.text);
 			if (json.error == null)
 			{
 				sceneChanger.LoadScene("Map Scene");
@@ -58,6 +56,6 @@ public class Login : MonoBehaviour
 		else if (password.text == "")
 			errorSubtitle.text = "Password cannot be empty";
 		else
-			StartCoroutine(GetCredential());
+			StartCoroutine(LoginCoroutine());
 	}
 }
--- Assets/Scripts/Register.cs
+++ Assets/Scripts/Register.cs
@@ -1,16 +1,82 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Register : MonoBehaviour {
+public class Register : MonoBehaviour
+{
+	string registerURL = "http://webcity.online/live/db/register.php?username={0}&pwhash={1}&email={2}";
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField]
+	InputField email = null,
+		username = null,
+		password = null,
+		confirmPw = null;
+	[SerializeField]
+	Text errorSubtitle = null;
+
+	[SerializeField]
+	ChangeScene sceneChanger = null;
+
+	IEnumerator RegisterCoroutine()
+	{
+		errorSubtitle.text = "";
+		string pwhash = PBKDF2Hash.Hash(password.text);
+		string url = string.Format(registerURL,
+			WWW.EscapeURL(username.text), 
+			WWW.EscapeURL(pwhash), 
+			WWW.EscapeURL(email.text));
+		WWW www = new WWW(url);
+		yield return www;
+
+		if (www.error != null)
+		{
+			errorSubtitle.text = www.error;
+		}
+		else
+		{
+			Login.User json = JsonUtility.FromJson<Login.User>(www.text);
+			if (json.error == null)
+			{
+				sceneChanger.LoadScene("Map Scene");
+			}
+			else
+			{
+				errorSubtitle.text = json.error;
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void OnValueChangedConfirmPassword()
+	{
+		if (password.text != confirmPw.text)
+			confirmPw.image.color = Color.HSVToRGB(0f, 0.1f, 1f);
+		else
+			confirmPw.image.color = Color.HSVToRGB(1f / 3f, 0.1f, 1f);
+	}
+
+	public void OnClickRegister()
+	{
+		// check email format
+		bool invalidEmail = false;
+		try { new System.Net.Mail.MailAddress(email.text); }
+		catch (FormatException) { invalidEmail = true; }
+
+		if (email.text == "")
+			errorSubtitle.text = "Email cannot be empty";
+		else if (invalidEmail)
+			errorSubtitle.text = "Invalid email address";
+		else if (username.text == "")
+			errorSubtitle.text = "Username cannot be empty";
+		else if (password.text == "")
+			errorSubtitle.text = "Password cannot be empty";
+		else if (confirmPw.text == "")
+			errorSubtitle.text = "Please confirm password";
+		else if (password.text != confirmPw.text)
+			errorSubtitle.text = "Passwords entered are not the same";
+		else
+			StartCoroutine(RegisterCoroutine());
 	}
 }
